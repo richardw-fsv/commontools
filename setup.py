bd4c85d4f5711f108f3ce13f5f7b5cc7f640431a
Richard West <richardw@afk.no>
2026-09-26 21:06:29 +0200

    Updates setup.py to read requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+# Function to read requirements from requirements.txt
+def read_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
 setup(
     name="rwcommontools",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=read_requirements(),
     author="RWest",
     description="A collection of common tools",
     python_requires=">=3.8",
